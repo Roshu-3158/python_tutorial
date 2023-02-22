ccd5a3428c22e3134f70178d41f1ec6463d7e562
Roshan Dattu Nikam <120217863+Roshu-3158@users.noreply.github.com>
2023-02-22 18:26:37 +0530

    Add files via upload

---
+++ _16_in_function.py
@@ -0,0 +1,6 @@
+list = [43,54,57,35,87]
+print(57 in list)
+print(72 in list)
+print(59 not in list)
+if 43 in list:
+    print("yes it is i list")
---
+++ _17_chake_age.py
@@ -0,0 +1,9 @@
+print("Enter your age")
+a = int(input())
+
+if a<18:
+    print("You are not eligible for drive")
+elif a==18:
+    print("come physically we wants to confirm")
+else: 
+    print("You are eligible to drive")
